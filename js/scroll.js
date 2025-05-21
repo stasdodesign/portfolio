d9a2e94226ee72a3ac0a5100fdca5921101ba7ad
Your Name <stasdodesign@ya.ru>
2025-05-21 23:49:39 +0300

    Initial commit with themes and styles

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('section'); // Все секции с id
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 60; // с запасом для меню
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === '#' + current) {
+      link.classList.add('active');
+    }
+  });
+});
